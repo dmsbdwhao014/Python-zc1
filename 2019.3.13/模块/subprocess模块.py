50daebefb56152934dc3116c496aa7cce25f405a
dmsbdwhao014 <z9l1v8t@foxmail.com>
2019-04-15 22:48:58 +0800

    home 4/19 22:49

--- 2019.3.13/模块/subprocess模块.py
+++ 2019.3.13/模块/subprocess模块.py
@@ -3,7 +3,8 @@ import subprocess
 # result = subprocess.call('ipconfig')
 # print(result)
 
-subprocess.check_call(["ipconfig"])
-subprocess.check_call("exit 1",shell=True)
-1
+result = subprocess.check_output('ipconfig')
+print(result)
 
+# subprocess.check_call(["ipconfig"])
+# subprocess.check_call("exit 1",shell=True)
